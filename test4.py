d87ff9bb98e2f5dcb762d708f926eb013aba4e3b
Juan Pablo Borrero <jpborrero@juans-air.dhcp.nd.edu>
2017-04-23 18:57:29 -0400

    Same as previous file but with comments

--- test4.py
+++ test4.py
@@ -1,16 +1,19 @@
-#Tweepy
+# This program retrieves the number of mentions of a company in twitter for
+#each day over the past week. The tweets are saved in tweets.txt file and the
+#number of mentions per day in data.txt file.
+
 import tweepy
 from datetime import timedelta, date
 import sys
 import jsonpickle
 import os
 
-maxTweets = 10000000 # Some arbitrary large number
-tweetsPerQry = 100  # this is the max the API permits
+maxTweets = 1000000 # Some arbitrary large number
+tweetsPerQry = 100  # this is the max the twitter API permits
 fName = 'tweets.txt' # We'll store the tweets in a text file.
-fName2 = 'data.txt'
-# Replace the API_KEY and API_SECRET with your application's key and secret.
-API_KEY = 'rF1UhAhVJsmie822K1bXN5lvd'
+fName2 = 'data.txt'  # Where the number of tweets per day will be saved
+
+API_KEY = 'rF1UhAhVJsmie822K1bXN5lvd' # To access the twitter API
 API_SECRET = 'UykH43jgHn5SWYHTprLyfrQPWKl7V2ODu2MhJVTqIJosjbLn5a'
 auth = tweepy.AppAuthHandler(API_KEY, API_SECRET)
 api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
@@ -19,10 +22,11 @@ if (not api):
     print ("Can't Authenticate")
     sys.exit(-1)
 
-searchQuery = '#' # this is what we're searching for
+# The company we are searching for
+searchQuery = '#'
 searchQuery += raw_input("Please enter the company name you want to search for: ")
 
-# Enter the date range
+# The date range we are searching for
 print "Enter today's date"
 day_string = raw_input("Day (dd): ")
 day = int(day_string)
@@ -39,29 +43,24 @@ to_date = from_date + delta
 
 with open(fName, 'w') as f:
 	saveFile = open ('data.txt', 'w')
+	# Loop to navigate through the week to search for the number of tweets per day
 	while to_date <= end_date:
-		print from_date.strftime("%Y-%m-%d")
-		print to_date.strftime("%Y-%m-%d")
-		# If results from a specific ID onwards are reqd, set since_id to that ID.
-		# else default to no lower limit, go as far back as API allows
-		sinceId = None
-		# If results only below a specific ID are, set max_id to that ID.
-		# else default to no upper limit, start from the most recent tweet matching the search query.
-		max_id = -1L
-		tweetCount = 0
+		tweetCount = 0 # To keep track of the number of tweets
+		sinceId = None # If results from a specific ID onwards are reqd, set since_id to that ID. Else default to no lower limit, go as far back as API allows
+		max_id = -1L # If results only below a specific ID are, set max_id to that ID. else default to no upper limit, start from the most recent tweet matching the search query.
 		print("Downloading max {0} tweets".format(maxTweets))
 		while tweetCount < maxTweets:
 			try:
 				if (max_id <= 0):
 					if (not sinceId):
-						new_tweets = api.search(q=searchQuery, count=tweetsPerQry, since=from_date , until=to_date)#, result_type = 'popular')
+						new_tweets = api.search(q=searchQuery, count=tweetsPerQry, since=from_date , until=to_date)
 					else:
-						new_tweets = api.search(q=searchQuery, count=tweetsPerQry, since_id=sinceId, since=from_date , until=to_date)#, result_type = 'popular'
+						new_tweets = api.search(q=searchQuery, count=tweetsPerQry, since_id=sinceId, since=from_date , until=to_date)
 				else:
 					if (not sinceId):
-						new_tweets = api.search(q=searchQuery, count=tweetsPerQry, max_id=str(max_id - 1), since=from_date , until=to_date)#, result_type = 'popular')
+						new_tweets = api.search(q=searchQuery, count=tweetsPerQry, max_id=str(max_id - 1), since=from_date , until=to_date)
 					else:
-						new_tweets = api.search(q=searchQuery, count=tweetsPerQry, max_id=str(max_id - 1), since_id=sinceId, since=from_date , until=to_date)#, result_type = 'popular')
+						new_tweets = api.search(q=searchQuery, count=tweetsPerQry, max_id=str(max_id - 1), since_id=sinceId, since=from_date , until=to_date)
 
 				if not new_tweets:
 					print("No more tweets found")
@@ -75,8 +74,7 @@ with open(fName, 'w') as f:
 				max_id = new_tweets[-1].id
 
 			except tweepy.TweepError as e:
-				# Just exit if any error
-				print("some error : " + str(e))
+				print("Some error : " + str(e))
 				break
 
 		print("There were: ", tweetCount, " tweets for ", from_date.strftime("%Y-%m-%d"))
