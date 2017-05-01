e3adc4a5133652674c378a2456a3c643913248a8
Noelle Rosa <noellerosa@noelles-mbp.dhcp.nd.edu>
2017-05-01 12:31:02 -0400

    runrunrun

--- run.py
+++ run.py
@@ -10,7 +10,7 @@ content = [x.strip() for x in content]
 content = [x.split(",") for x in content]
 stockDay= []
 stockPrice = []
-for y in content:
+for y in reversed(content):
 	stockDay.append(y[0])
 	stockPrice.append(y[1])
 print (stockDay)
@@ -21,11 +21,49 @@ with open('data.csv', 'r') as csv2:
 	newcont = csv2.readlines()
 newcont = [n.strip() for n in newcont]
 newcont = [n.split(",") for n in newcont]
-tweetDay= []
-tweetPrice = []
+tempTweetDay= []
+tempTweetNum = []
 for z in newcont:
-	tweetDay.append(z[0])
-	tweetPrice.append(z[1])
-print (tweetDay)
-print (tweetPrice)
+	tempTweetDay.append(z[0])
+	tempTweetNum.append(z[1])
+print (tempTweetDay)
+print (tempTweetNum)
 
+#NEED TO REDUCE TWEETDAYLIST AND TWEETLIST
+tweetNum = []
+counter=0
+looper=0
+for l in tempTweetDay:
+	print l
+	print stockDay[counter]
+	if l is stockDay[counter]:
+		print "in it"
+		tweetNum.append(tempTweetNum[looper])
+		counter = counter+1
+	looper = looper+1
+
+print (tweetNum)
+
+exit()
+
+#PLOTTING
+plt.figure(1)
+plt.subplot(211)
+
+plt.title('Tweets on Company per Day')
+plt.xlabel('Date')
+plt.ylabel('Number of tweets')
+plt.grid(True)
+plt.bar(dayList, tweetNum, 0.5, color='blue')
+plt.tight_layout()
+
+
+plt.figure(1)
+plt.subplot(212)
+plt.title('Closeing Stock Price per Day')
+plt.xlabel('Date')
+plt.ylabel('Closing Stock price($)')
+plt.grid(True)
+plt.plot(stockDay, stockPrice, 'g')
+plt.tight_layout()
+plt.show()
